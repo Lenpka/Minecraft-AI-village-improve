9ddf947f7d4c4b3468074ae32e36665895771f3c
Leonid Konstantinov <leanidkonstantinov@yandex.ru>
2025-01-27 11:11:06 +0300

    start

---
+++ src/main/java/aivil/aivillagerfabric/Aivillagerfabric.java
@@ -0,0 +1,10 @@
+package aivil.aivillagerfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Aivillagerfabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/aivil/aivillagerfabric/client/AivillagerfabricClient.java
@@ -0,0 +1,10 @@
+package aivil.aivillagerfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AivillagerfabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
